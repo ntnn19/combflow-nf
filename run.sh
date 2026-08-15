d0870a4a2566089e854c91d31a71253182b31dca
ntnn19 <natannagar89@gmail.com>
2026-08-16 00:02:24 +0200

    cleaning repo

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-nextflow run main.nf -profile local_apptainer,local_conda --fasta assets/example.fasta \
-    --pdb_dir /scratch/home/nagarnat/projects/scaturro/zika_ns5/replication_complex/pdb --stoichiometry 'NS1:1-2,NS2A:1-2'
+nextflow run main.nf -profile slurm,apptainer,conda --fasta assets/example.fasta \
+    --pdb_dir /scratch/home/nagarnat/projects/scaturro/zika_ns5/replication_complex/pdb \
+    --stoichiometry 'NS1:1-5,NS2A:1-5,NS2B:1-5,NS3:1-5,NS4A:1-5,NS4B:1-5,NS5:1-5' -resume
